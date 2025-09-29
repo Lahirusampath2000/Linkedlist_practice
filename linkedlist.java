b0e32b2bc65e08974901074afed0834167dc1198
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2025-09-29 11:56:07 +0530

    print specific index

--- linkedlist.java
+++ linkedlist.java
@@ -23,6 +23,7 @@ public class linkedlist {
 
 
     }
+    // display all value
 
     public void display(){
         node current=head;
@@ -30,6 +31,24 @@ public class linkedlist {
             System.out.println(current.data);
             current=current.next;
         }
+    }
+    //display specific index value
+    public void printspecificvalue(int index){
+        node current=head;
+        int count=0;
+        if(index<0){
+            System.out.println("Invalid index");
+            return;
+        }
+        while(current!=null){
+            if(count==index){
+                System.out.println("Node at postion "+ index+" is "+current.data);
+                return;
+            }
+            current=current.next;
+            count++;
+        }
+        System.out.println("index out of range");
     }
 
 }
