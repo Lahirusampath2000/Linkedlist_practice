8b25552f5c3bd5a1d1f558409022c160579f846b
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2025-09-29 11:41:49 +0530

    created new linked list

--- linkedlist.java
+++ linkedlist.java
@@ -5,4 +5,22 @@ public class linkedlist {
     public linkedlist(){
         head = null;
     }
+
+    public void insert(int data){
+        node newNode=new node(data);
+
+        if(head==null){
+            head=newNode;
+            return;
+        }
+
+        node current=head;
+        while (head.next != null) {
+            current=current.next;
+
+        }
+        current.next=newNode;
+
+
+    }
 }
---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        linkedlist newlinkedlist = new linkedlist();
+
+        newlinkedlist.insert(10);
+        newlinkedlist.insert(20);
+        newlinkedlist.insert(30);
+        newlinkedlist.insert(40);
+        newlinkedlist.insert(50);
+    }
+}
